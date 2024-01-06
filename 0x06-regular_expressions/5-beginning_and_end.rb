7ca89ef2ecc7db5ca3fb75ac6832f376bb18e8aa
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-01-06 23:56:19 +0000

    match wildcards

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+# This code finds pattern based on repetition
+# should match h and anything to n
+
+if ARGV.length == 1
+  regex = /h.*n/
+
+  matches = ARGV[0].scan(regex)
+  if matches.empty?
+    puts "\n"
+  else
+    puts matches.join('')
+  end
+end
