81568c3f11d6e82e4e8203cba5ba40bd4d43e9d5
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-01-06 23:21:13 +0000

    [fix] refactor

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/env ruby
-[A-Z]+\olberton
+# This code matches the regex of Holberton and School
+
+
+if ARGV.length == 1
+  regex = /Holberton|School/
+
+  matches = ARGV[0].scan(regex)
+  if matches.empty?
+    puts "\n"
+  else
+      puts matches.join('')
+  end
+end
