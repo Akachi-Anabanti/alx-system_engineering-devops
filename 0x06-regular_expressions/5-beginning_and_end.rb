84ff473edf1ff9862d3a031193539007cff2aa9d
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-01-07 00:09:47 +0000

    refactor on regex to match single occurrence

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 # This code finds pattern based on repetition
 # should match h and anything to n
+# should match h9n but not h99n and not hn
 
 if ARGV.length == 1
-  regex = /h.+n/
+  regex = /h.?+n/
 
   matches = ARGV[0].scan(regex)
   if matches.empty?
