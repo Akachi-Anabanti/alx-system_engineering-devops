2f9e79a9f9669d1e633a7c9044de57e4c80f3d88
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-04-02 14:49:12 +0000

    all employees

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""A script that accepts user ID and returns
+Associated data"""
+import csv
+import json
+import sys
+import urllib.request
+
+
+def get_all():
+    """gets all user info"""
+
+    url_users_info = "https://jsonplaceholder.typicode.com/users/"
+
+    try:
+        with urllib.request.urlopen(url_users_info) as response:
+            users_info = response.read().decode("utf-8")
+        users_info = json.loads(users_info)
+    except Exception as e:
+        print(e)
+        return
+
+    for user in users_info:
+        print(type(user.get("id")))
+        list_of_tasks = to_json(user.get("id"))
+        info = {user.get("id"): list_of_tasks}
+        with open("todo_all_employees.json", "a") as file:
+            json.dump(info, file)
+
+
+def to_json(user_id):
+    """a function that performs the API call"""
+    url_tasks = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+    url_user_info = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    try:
+        with urllib.request.urlopen(url_user_info) as response:
+            user_info = response.read().decode("utf-8")
+        user_info = json.loads(user_info)
+    except Exception as e:
+        print(e)
+        return
+
+    try:
+        with urllib.request.urlopen(url_tasks) as response:
+            data = response.read().decode('utf-8')
+        data = json.loads(data)
+    except Exception as e:
+        print(e)
+        return
+    list_of_tasks = [
+                        {
+                            "username": user_info.get("username"),
+                            "task": task.get("title"),
+                            "completed": task.get("completed"),
+                        }
+                        for task in data
+                    ]
+
+    return list_of_tasks
+
+
+if __name__ == "__main__":
+    get_all()
