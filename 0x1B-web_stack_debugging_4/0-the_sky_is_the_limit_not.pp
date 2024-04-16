cead13ea61a303618d5b444a1056016cb5debe3f
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-04-16 15:14:06 +0000

    puppet fix

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Fixing the max file opened error by 
+
+# increasing the ULIMIT in the default file
+
+exec {'set-ulimit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/bin/local/bin/:/bin/'
+}
+# Restart
+exec {'restart-nginx':
+  command => 'service nginx restart',
+  user    => 'root'
+}
